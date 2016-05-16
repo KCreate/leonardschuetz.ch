539f168c05978ffb735949df0b98b2e7cc0b0a36
Leonard Schuetz <leni.schuetz@me.com>
2016-05-16 23:43:09 +0200

    Added paralax effect to header

--- client/components/Header.js
+++ client/components/Header.js
@@ -13,6 +13,7 @@ class Header extends Component {
 
     constructor(...args) {
         super(...args);
+        this.paralaxHandler = this.paralaxHandler.bind(this);
 
         this.state = Object.assign({}, this.props, {
             title: '',
@@ -21,6 +22,10 @@ class Header extends Component {
     }
 
     componentDidMount() {
+
+        if (window) {
+            window.addEventListener('scroll', this.paralaxHandler);
+        }
 
         let i = 0;
         setTimeout(() => {
@@ -43,6 +48,45 @@ class Header extends Component {
         }, 100);
     }
 
+    componentWillUnmount() {
+        if (window) {
+            window.removeEventListener('scroll', this.paralaxHandler);
+        }
+    }
+
+    paralaxHandler(event) {
+
+        let x, y;
+
+        // Reference: https://developer.mozilla.org/de/docs/Web/API/Window/scrollY
+        const supportPageOffset = (window.pageXOffset !== undefined && window.pageYOffset !== undefined);
+        const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat');
+        if (supportPageOffset) {
+            x = window.pageXOffset;
+            y = window.pageYOffset;
+        } else {
+            if (isCSS1Compat) {
+                x = document.documentElement.scrollLeft;
+                y = document.documentElement.scrollTop;
+            } else {
+                x = document.body.scrollLeft;
+                y = document.body.scrollTop;
+            }
+        }
+
+        // Paralax modifier
+        const modifier = 0.25;
+        const xModified = x * modifier;
+        const yModified = y * modifier;
+
+        // Only translate if the Header is physically on the screen
+        if (y <= 205 * (1 + modifier)) {
+            if (this.refs.Header) {
+                this.refs.Header.style.transform = 'translateY(' + yModified + 'px)';
+            }
+        }
+    }
+
     render() {
 
         function shouldInclude(value) {
@@ -50,7 +94,7 @@ class Header extends Component {
         }
 
         return (
-            <div className={classnames({ Header: true, expanded: this.props.expanded })}>
+            <div className={classnames({ Header: true, expanded: this.props.expanded })} ref="Header">
                 <div>
                     <h1><Link to="/">{this.state.title}</Link></h1>
                     <ul>
