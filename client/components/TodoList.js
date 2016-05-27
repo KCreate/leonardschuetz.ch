8462180a183cecd3a3698f75987a76ed134d1f20
Leonard Schuetz <leni.schuetz@me.com>
2016-05-27 23:45:36 +0200

    Show todos as links if set so

--- client/components/TodoList.js
+++ client/components/TodoList.js
@@ -5,14 +5,23 @@ import Card from './Card';
 import './../style/TodoList.scss';
 import './../style/List.scss';
 class TodoList extends Component {
+
+    constructor(...args) {
+        super(...args);
+        this.onRemove = this.onRemove.bind(this);
+    }
+
+    onRemove(index) {
+        this.props.oncomplete(index);
+    }
+
     render() {
         return (
             <ul className="List">
                 {this.props.todos.map((todo, index) => (
                     <li key={index} className="ListItem">
-                        {todo.text}<button onClick={() => {
-                            this.props.oncomplete(index);
-                        }}>Remove</button>
+                        {(todo.isLink ? <a href={todo.text}>{todo.text}</a> : todo.text)}
+                        <button onClick={this.onRemove.bind(this, index)}>Remove</button>
                     </li>
                 ))}
             </ul>
