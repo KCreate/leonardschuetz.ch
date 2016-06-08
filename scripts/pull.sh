3a0388c2b85bc3917c1c4be6d39e986bff3fa429
Leonard Schuetz <leni.schuetz@me.com>
2016-06-08 18:28:56 +0200

    Fixed incorrect error

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -5,3 +5,7 @@ git pull origin master
 npm run build
 pm2 restart server
 git stash pop
+
+# if there is nothing to pop, git stash returns 1 as a status code
+# manually return 0 so npm won't complain
+exit 0
