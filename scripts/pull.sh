abfe55645a29e036df0a206e6859a40faf1f4986
Leonard Schuetz <leni.schuetz@me.com>
2016-06-12 23:34:40 +0200

    Reverted last commit

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# Backup the dist folder, since admins can upload data directly to it
-cp -r ../dist ./.tmpBackup
-
 git stash
 git pull origin master
 npm run build
 pm2 restart server
 git stash pop
-
-# Merge the backup with the new dist folder, ignoring existing files
-rsync -a --ignore-existing ./.tmpBackup ../dist
-
-# Delete the backups
-rm -rf ./.tmpBackup
 
 # if there is nothing to pop, git stash returns 1 as a status code
 # manually return 0 so npm won't complain
