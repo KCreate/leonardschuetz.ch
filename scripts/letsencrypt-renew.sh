1f7ef8a3a80986f018ee2791528b845a6ed1d0d5
Leonard Schuetz <leni.schuetz@me.com>
2016-07-31 19:14:00 +0200

    Changed update and server process

--- scripts/letsencrypt-renew.sh
+++
@@ -1,6 +0,0 @@
-hash certbot 2>/dev/null || {
-    echo >&2 "I require certbot but it's not installed.";
-    exit 1;
-}
-
-certbot certonly --webroot -w /var/github/KCreate/leonardschuetz.ch/dist -d leonardschuetz.ch
--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 git stash
+git checkout master
 git pull origin master
 npm run build
-pm2 restart server
+sudo pm2 restart process.json
 git stash pop
-
-# if there is nothing to pop, git stash returns 1 as a status code
-# manually return 0 so npm won't complain
-exit 0
