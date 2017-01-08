734b74fcf0801abb6ac611e1c622c356b4ec24d4
Leonard Schuetz <leni.schuetz@me.com>
2017-01-08 05:55:18 +0100

    Added google analytics

--- client/app/boot.js
+++ client/app/boot.js
@@ -45,3 +45,14 @@ window.addEventListener('load', (event) => {
         window.scrollTo(0, 0);
     }, 10);
 });
+
+/*
+ * Google Analytics
+ * */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-89960492-1', 'auto');
+ga('send', 'pageview');
