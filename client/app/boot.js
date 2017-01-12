11960ad19d929f8e98173b924afb78b65268ed5a
Leonard Schütz <leonard.schuetz@siemens.com>
2017-01-12 08:45:36 +0100

    Added Object.assign polyfill for IE

--- client/app/boot.js
+++ client/app/boot.js
@@ -6,6 +6,33 @@ import './index.html';
 import style from '../style/master.scss';
 import favicon from './favicon.ico';
 
+// Internet Explorer ...
+// Source: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+if (typeof Object.assign != 'function') {
+  Object.prototype.assign = function (target, varArgs) { // .length of function is 2
+    'use strict';
+    if (target == null) { // TypeError if undefined or null
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) { // Skip over if undefined or null
+        for (var nextKey in nextSource) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
 import App from '../components/App.js';
 import FrontPageController from '../components/FrontPageController';
 import TodosController from '../components/TodosController';
