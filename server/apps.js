277478ce3ae44d5a52103513c5dc023a8ea6626e
Leonard Schuetz <leni.schuetz@me.com>
2017-10-04 11:24:41 +0200

    Connect 4 game backend

--- server/apps.js
+++ server/apps.js
@@ -3,5 +3,6 @@ const express   = require('express');
 const router    = new express.Router();
 
 router.use('/txt-png', require('./apps/txt-png'));
+router.use('/connect4', require('./apps/connect4'));
 
 module.exports = router;
