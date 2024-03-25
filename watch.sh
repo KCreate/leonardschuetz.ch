79bfa18da1f41b5faef3fb7f6d68ed7267b388ef
Leonard Schütz <leni.schuetz@me.com>
2024-03-25 23:42:28 +0100

    Updates

--- watch.sh
+++ watch.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-CSS_PATHS=
-while inotifywait --quiet --event close_write \
-  server/resources/css/*.scss                 \
-  server/blog/*/*.md                          \
-  server/blog/*/*/*.md                        \
-  ; do ./build.sh; done
+
+while sleep 1; do ./build.sh; done
+
+# while inotifywait --quiet --event close_write \
+#   server/resources/css/*.scss                 \
+#   server/blog/*/*.md                          \
+#   server/blog/*/*/*.md                        \
+#   ; do ./build.sh; done
